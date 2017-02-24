e45c708bde59119c1b602af3c0c952e8d5bd3ca4
Arin Singh <Arin.Singh@derivco.co.za>
2017-02-24 14:35:39 +0200

    First Commit

---
+++ SRChat/SRChat/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name,string message)
+        {
+            //broadcastMessage updates the clients
+            Clients.All.broadcastMessage(name,message);
+        }
+    }
+}
---
+++ SRChat/SRChat/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SRChat.Startup))]
+
+namespace SRChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //insert configurations for the connect or hub wire ups
+            app.MapSignalR();
+        }
+    }
+}
